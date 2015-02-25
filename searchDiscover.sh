ba3e90fe980ca732d5883f4f41141b7593f50000
eltonngjela@thedancingbits.com <eltonngjela@thedancingbits.com@935d5466-a334-169a-0507-d06f8d43d3dc>
2015-02-25 13:29:57 +0000

    ability to write on text files the output of the functions

--- searchDiscover.sh
+++ searchDiscover.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Linux Administration - FSHN.edu.al - Fall 2015, Semestral Project
 # Team Members: Roberta B., Nikolin N., Elton N.
-# Revision 13, Version 0.4
+# Revision 14, Version 0.5
+
+#GLOBAL VARIABLES
+writeToFile=0
+#END GLOBAL VARIABLES
 
 #FUNCTION_DECLARATION
 function takeBrake(){
@@ -14,13 +18,28 @@ function returnGeoIP(){
 	geoIPCommandGlobal=${geoIPCommandGlobal/GeoIP ASNum Edition: /|}
 }
 function showActiveUsers(){
-	w
+	if [ "$writeToFile" -eq 1 ]; then
+		w >> logs.txt
+	else
+		w
+	fi
+	writeToFile=0
 }
 function showOpenFirewallPorts(){
-	netstat -lnptu
+	if [ "$writeToFile" -eq 1 ]; then
+		netstat -lnptu >> logs.txt
+	else
+		netstat -lnptu
+	fi
+	writeToFile=0
 }
 function showRAMProcesses(){
-	ps aux | sort -rn -k 5,6
+	if [ "$writeToFile" -eq 1 ]; then
+		ps aux | sort -rn -k 5,6 >> logs.txt
+	else
+		ps aux | sort -rn -k 5,6
+	fi
+	writeToFile=0
 	printf "\033[01;35m $(tput setab 3) NOTE: Processes are filtered based on: $(tput sgr 0) \033[0m\n"
 	printf "\033[01;35m $(tput setab 3) 1) RAM usage $(tput sgr 0) \033[0m\n"
 	printf "\033[01;35m $(tput setab 3) 2) CPU usage $(tput sgr 0) \033[0m\n"
@@ -36,8 +55,13 @@ function loopThroughInvalidLoogins(){
 		read -a geoIpArray <<< "${geoIPCommand///}"
 		IFS=$oldIFS
 		
-		printf "USERNAME ATTEMPT: \033[00;31m ${ipArray[2]} \033[0m from \033[00;32m ${geoIpArray[1]: (-2)} \033[0m:\033[00;32m ${geoIpArray[4]} \033[0m with IP: \033[00;32m ${ipArray[4]} \033[0m on coordinates LAT: \033[01;33m ${geoIpArray[6]} \033[0m and LON: \033[01;33m ${geoIpArray[7]} \033[0m\n"
+		if [ "$writeToFile" -eq 1 ]; then
+			printf "USERNAME ATTEMPT: \033[00;31m ${ipArray[2]} \033[0m from \033[00;32m ${geoIpArray[1]: (-2)} \033[0m:\033[00;32m ${geoIpArray[4]} \033[0m with IP: \033[00;32m ${ipArray[4]} \033[0m on coordinates LAT: \033[01;33m ${geoIpArray[6]} \033[0m and LON: \033[01;33m ${geoIpArray[7]} \033[0m\n" >> logs.txt
+		else
+			printf "USERNAME ATTEMPT: \033[00;31m ${ipArray[2]} \033[0m from \033[00;32m ${geoIpArray[1]: (-2)} \033[0m:\033[00;32m ${geoIpArray[4]} \033[0m with IP: \033[00;32m ${ipArray[4]} \033[0m on coordinates LAT: \033[01;33m ${geoIpArray[6]} \033[0m and LON: \033[01;33m ${geoIpArray[7]} \033[0m\n"
+		fi
 	done
+	writeToFile=0
 }
 
 function getApacheSuccessfulConnections(){
@@ -61,10 +85,15 @@ function getApacheSuccessfulConnections(){
 		
 		read -a geoTrackingResults <<< "$geoIPCommandGlobal"
 		
-		printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n"
+		if [ "$writeToFile" -eq 1 ]; then
+			printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n" >> logs.txt
+		else
+			printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n"
+		fi
 		
 		IFS=$oldIFS
 	done
+	writeToFile=0
 }
 
 function getApacheUnsuccessfulConnections(){
@@ -89,10 +118,15 @@ function getApacheUnsuccessfulConnections(){
 		
 		read -a geoTrackingResults <<< "$geoIPCommandGlobal"
 		
-		printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n"
+		if [ "$writeToFile" -eq 1 ]; then
+			printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n" >> logs.txt
+		else
+			printf "\033[00;32m ${ipArray[1]/[/ }\033[0m, \033[01;33m${geoTrackingResults[1]}\033[0m, \033[00;32m$IP\033[0m, ${ipArray[2]}, \033[01;34m$(tput setab 7)${geoTrackingResults[2]}$(tput sgr 0)\033[0m \n"
+		fi
 		
 		IFS=$oldIFS
 	done
+	writeToFile=0
 }
 
 function searchOnFilesForPaterns(){
@@ -119,7 +153,14 @@ function searchOnFilesForPaterns(){
 	printf ":::your final query looks like: \033[00;32m  grep -rw -i --include=*.{$fileExtentionsToCheck} \"$keywordForPaternToCheck\" / \033[0m \n\n"
 	
 	local grepExecutionCommand="$(eval grep -rw -i --include=*.{${fileExtentionsToCheck}} \"${keywordForPaternToCheck}\" /)"
-	printf "${grepExecutionCommand} \n"
+	
+	if [ "$writeToFile" -eq 1 ]; then
+		printf "${grepExecutionCommand} \n" >> logs.txt
+	else
+		printf "${grepExecutionCommand} \n"
+	fi
+	
+	writeToFile=0
 }
 
 function findAllFilesWithLessThan5LinesCode(){
@@ -136,17 +177,32 @@ function findAllFilesWithLessThan5LinesCode(){
 		#currently only counts all lines on all files, will be updated soon
 		local wExecutionCommand="$(eval wc -l ${f})"
 		printf "${wExecutionCommand} \n"
+		
+		if [ "$writeToFile" -eq 1 ]; then
+			printf "${wExecutionCommand} \n" >> logs.txt
+		else
+			printf "${wExecutionCommand} \n"
+		fi
 	done
+	writeToFile=0
 }
 
 function searchX__patterInsideGivenDirectory(){
+	#TESTED PATH /home/ubuntu/wordpress/*
 	printf ":::provide file path to search: \n"
 	read pathToSearch
 	
-	local grepExecutionCommand="$(eval grep -w '\\[xX][0-9][0-9]' $pathToSearch)"
+	local grepExecutionCommand="$(eval grep -rw "\\x[0-9][0-9]" $pathToSearch)"
 	printf "${grepExecutionCommand} \n"
+	
+	if [ "$writeToFile" -eq 1 ]; then
+		printf "${grepExecutionCommand} \n" >> logs.txt
+	else
+		printf "${grepExecutionCommand} \n"
+	fi
+	writeToFile=0
 }
-#END_FUNCTION_DECLARITION
+#END_FUNCTION_DECLARATION
 
 #EXECUTION_TIME
 clear
@@ -156,65 +212,71 @@ varTitle="Starting searchDiscover execution now for user: $USER"
 printf $varTitle
 printf "\n"
 
-printf "\033[01;91m $(tput setab 1) LIST OF CONNECTED USERS $(tput sgr 0) \033[0m\n"
-printf "\n"
-showActiveUsers
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 2) OPEN FIREWALL PORTS $(tput sgr 0) \033[0m\n"
-printf "\n"
-showOpenFirewallPorts
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 3) SHORTING PROCESSES IN RAM $(tput sgr 0) \033[0m\n"
-printf "\n"
-showRAMProcesses
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 4) LOOP THROUGH UNSUCCESSFUL LOGIN ATTPETS $(tput sgr 0) \033[0m\n"
-printf "\n"
-loopThroughInvalidLoogins
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 5) SUCCSESSFUL APACHE CONNECTIONS (200) $(tput sgr 0) \033[0m\n"
-printf "\n"
-getApacheSuccessfulConnections
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 6) UNSUCCSESSFUL APACHE CONNECTIONS (404) $(tput sgr 0) \033[0m\n"
-printf "\n"
-getApacheUnsuccessfulConnections
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 7) SEARCH TEXT PATTERN(s) OVER ALL FILES $(tput sgr 0) \033[0m\n"
-printf "\n"
-searchOnFilesForPaterns
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 8) FIND ALL FILES WITH LESS THAN 5 LINES $(tput sgr 0) \033[0m\n"
-printf "\n"
-findAllFilesWithLessThan5LinesCode
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
-
-printf "\033[01;91m $(tput setab 9) SEARCH \x__ PATTERN OVER ALL FILES INSIDE A DIRECTORY $(tput sgr 0) \033[0m\n"
-printf "\n"
-searchX__patterInsideGivenDirectory
-printf "\n"
-
-takeBrake '[Enter] to dig further...'
+while true; do
+	printf "1) LIST OF CONNECTED USERS \n"
+	printf "2) OPEN FIREWALL PORTS \n"
+	printf "3) SHORTING PROCESSES IN RAM \n"
+	printf "4) LOOP THROUGH UNSUCCESSFUL LOGIN ATTPETS \n"
+	printf "5) SUCCSESSFUL APACHE CONNECTIONS (200) \n"
+	printf "6) UNSUCCSESSFUL APACHE CONNECTIONS (404) \n"
+	printf "7) SEARCH TEXT PATTERN(s) OVER ALL FILES \n"
+	printf "8) FIND ALL FILES WITH LESS THAN 5 LINES \n"
+	printf "9) SEARCH x__ PATTERN OVER ALL FILES INSIDE A DIRECTORY \n"
+	printf "s) write function output on logs \n"
+	printf "0) quit \n"
+	read selection;
+	
+	if [ "$selection" == "1" ]; then
+		printf "\033[01;91m $(tput setab 7) LIST OF CONNECTED USERS $(tput sgr 0) \033[0m\n\n"
+		showActiveUsers
+		printf "\n"
+		takeBrake '1.done [Enter] to dig further...'
+	elif [ "$selection" == "2" ]; then
+		printf "\033[01;91m $(tput setab 7) OPEN FIREWALL PORTS $(tput sgr 0) \033[0m\n\n"
+		showOpenFirewallPorts
+		printf "\n"
+		takeBrake '2.done [Enter] to dig further...'
+	elif [ "$selection" == "3" ]; then
+		printf "\033[01;91m $(tput setab 7) SHORTING PROCESSES IN RAM $(tput sgr 0) \033[0m\n\n"
+		showRAMProcesses
+		printf "\n"
+		takeBrake '3.done [Enter] to dig further...'
+	elif [ "$selection" == "4" ]; then
+		printf "\033[01;91m $(tput setab 7) LOOP THROUGH UNSUCCESSFUL LOGIN ATTPETS $(tput sgr 0) \033[0m\n\n"
+		loopThroughInvalidLoogins
+		printf "\n"
+		takeBrake '4.done [Enter] to dig further...'
+	elif [ "$selection" == "5" ]; then
+		printf "\033[01;91m $(tput setab 7) SUCCSESSFUL APACHE CONNECTIONS (200) $(tput sgr 0) \033[0m\n\n"
+		getApacheSuccessfulConnections
+		printf "\n"
+		takeBrake '5.done [Enter] to dig further...'
+	elif [ "$selection" == "6" ]; then
+		printf "\033[01;91m $(tput setab 7) UNSUCCSESSFUL APACHE CONNECTIONS (404) $(tput sgr 0) \033[0m\n\n"
+		getApacheUnsuccessfulConnections
+		printf "\n"
+		takeBrake '6.done [Enter] to dig further...'
+	elif [ "$selection" == "7" ]; then
+		printf "\033[01;91m $(tput setab 7) SEARCH TEXT PATTERN(s) OVER ALL FILES $(tput sgr 0) \033[0m\n\n"
+		searchOnFilesForPaterns
+		printf "\n"
+		takeBrake '7.done [Enter] to dig further...'
+	elif [ "$selection" == "8" ]; then
+		printf "\033[01;91m $(tput setab 7) FIND ALL FILES WITH LESS THAN 5 LINES $(tput sgr 0) \033[0m\n\n"
+		findAllFilesWithLessThan5LinesCode
+		printf "\n"
+		takeBrake '8.done [Enter] to dig further...'
+	elif [ "$selection" == "9" ]; then
+		printf "\033[01;91m $(tput setab 7) SEARCH \x__ PATTERN OVER ALL FILES INSIDE A DIRECTORY $(tput sgr 0) \033[0m\n\n"
+		searchX__patterInsideGivenDirectory
+		printf "\n"
+		takeBrake '9.done [Enter] to dig further...'
+	elif [ "$selection" == "s" ]; then
+		writeToFile=1
+		printf "\n\n \033[00;32m writing memo set, choose to exec \033[0m \n\n"
+	elif [ "$selection" == "0" ]; then
+		break
+	else
+		printf "\n\n \033[00;32m [0-9] only \033[0m \n\n"
+	fi
+done
